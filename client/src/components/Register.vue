f9a30c4b000453314f27fcb40d049ecd0e966ec1
MAshraf <ee.mashraf@gmail.com>
2017-09-07 17:03:41 +0300

    Vuetify + fix Regex

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" v-model="email" placeholder="email" />
-    <br />
-    <input type="password"" name="password" v-model="password" placeholder="password" />
-    <br />
-    <button @click="register">Register</button>  
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>
+            Register
+          </v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pl-2 pb-2">
+          <v-text-field
+            label= "Email"
+            v-mode="email"
+            ></v-text-field>
+          <v-text-field
+            label= "Password"
+            v-mode="password"
+            ></v-text-field>
+          <div v-html="error" class="error"/>
+          <br />
+          <v-btn class="cyan" @click="register">Register</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -15,16 +31,20 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -32,5 +52,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.error{
+  color: red;
+}
+input[type="text"] {
+    border-bottom: 1px solid #333;
+}
 </style>
