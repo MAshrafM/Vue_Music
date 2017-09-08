23d7a18ae105c94e08de287bf64a8cece0bc6f2c
MAshraf <ee.mashraf@gmail.com>
2017-09-08 07:38:32 +0300

    login view

---
+++ client/src/components/Login.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>
+            Login
+          </v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pl-2 pb-2">
+          <v-text-field
+            label= "Email"
+            v-mode="email"
+            ></v-text-field>
+          <v-text-field
+            label= "Password"
+            v-mode="password"
+            ></v-text-field>
+          <div v-html="error" class="error"/>
+          <br />
+          <v-btn dark class="cyan" @click="login">Login</v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: null
+    }
+  },
+  methods: {
+    async login () {
+      try {
+        await AuthenticationService.login({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.error{
+  color: red;
+}
+</style>
