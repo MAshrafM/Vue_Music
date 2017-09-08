be5cabfcd4842b41458857de15cf91606985ea1b
MAshraf <ee.mashraf@gmail.com>
2017-09-08 07:50:37 +0300

    server auth login

--- server/src/routes.js
+++ server/src/routes.js
@@ -3,4 +3,6 @@ const AuthenticationControllerPolicy
  = require('./policies/AuthenticationControllerPolicy')
 module.exports = (app) => {
   app.post('/register', AuthenticationControllerPolicy.register,AuthenticationController.register)
+  
+  app.post('/login', AuthenticationController.login)
 }
