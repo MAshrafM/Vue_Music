454ee460acb2eb5211f49a14d34b695aebfec5e2
MAshraf <ee.mashraf@gmail.com>
2017-09-10 17:11:38 +0300

    songs models and controller

---
+++ server/src/controllers/SongsController.js
@@ -0,0 +1,28 @@
+const {Song} = require('../models')
+
+module.exports = {
+  // Index: Get All Songs
+  async index (req, res) {
+    try {
+      const songs = Song.findAll({
+        limit: 10
+      })
+      res.send(songs)
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to get All songs'
+      })
+    }
+  }
+  // Post: Create Songs
+  async post (req, res) {
+    try{
+      const song = Song.create(req.body)
+      res.send(song)
+    } catch (err){
+      res.status(500).send({
+        error: 'An error has occured trying to create a song'
+      })
+    }
+  }
+}
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,8 +1,19 @@
 const AuthenticationController = require('./controllers/AuthenticationController.js')
 const AuthenticationControllerPolicy
  = require('./policies/AuthenticationControllerPolicy')
+const SongsController = require('./controllers/SongsController')
+
 module.exports = (app) => {
+  //register a user end-point
+  // Authentication Rules/Polices
+  // Authentication Controller
   app.post('/register', AuthenticationControllerPolicy.register,AuthenticationController.register)
-  
+  // login end-point
+  // Login Controller
   app.post('/login', AuthenticationController.login)
+  // songs end-point
+  // GetAll
+  app.get('/songs', SongsController.index)
+  // Create a song end-point
+  app.post('/songs', SongsController.post)
 }
