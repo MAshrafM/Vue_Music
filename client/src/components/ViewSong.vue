5e6e45d8ffcd6260d9abf5d1d8e3547ac617c999
MAshraf <ee.mashraf@gmail.com>
2017-09-15 07:03:09 +0300

    client show song service

--- client/src/components/ViewSong.vue
+++ client/src/components/ViewSong.vue
@@ -4,7 +4,16 @@
 
 <script>
 export default {
-
+  import 'SongsService' from '@/services/SongsService'
+  data () {
+    return {
+      song: null
+    }
+  },
+  async mounted () {
+    const songId = this.$store.state.route.params.songId
+    this.song = await SongsService.show(songId)
+  }
 }
 </script>
 
