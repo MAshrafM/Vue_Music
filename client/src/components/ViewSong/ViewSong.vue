85b7840beffa9877193944c4abd66a3e68624109
MAshraf <ee.mashraf@gmail.com>
2017-09-16 08:38:02 +0300

    view music panel refactor

--- client/src/components/ViewSong/ViewSong.vue
+++ client/src/components/ViewSong/ViewSong.vue
@@ -1,26 +1,7 @@
 <template>
   <v-layout>
     <v-flex xs6>
-      <panel title="Song Metadata">
-        <v-layout>
-          <v-flex xs6>
-            <div class="song-title">
-              {{song.title}}
-            </div>
-            <div class="song-artist">
-              {{song.artist}}
-            </div>
-            <div class="song-genre">
-              {{song.genre}}
-            </div>
-          </v-flex>
-          <v-flex xs6>
-            <img class="album-cover" :src="song.albumImageUrl" />
-            <br>
-            {{song.album}}
-          </v-flex>
-        </v-layout>
-      </panel>
+      <song-metadata :song="song" />
     </v-flex>
     
     <v-flex xs6 ml-2>
