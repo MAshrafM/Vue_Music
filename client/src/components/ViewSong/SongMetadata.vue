dd8131d4745c1193b6eeb04df0aa3637101ca368
MAshraf <ee.mashraf@gmail.com>
2017-10-02 18:12:10 +0200

    client bookmark view and service

--- client/src/components/ViewSong/SongMetadata.vue
+++ client/src/components/ViewSong/SongMetadata.vue
@@ -26,6 +26,23 @@
         >
           Edit
         </v-btn>
+        
+        <v-btn 
+          v-if="isUserLoggedIn && !isBookmarked"
+          dark
+          class="cyan"
+          @click="bookmark"
+        >
+          Bookmark
+        </v-btn>
+        <v-btn 
+          v-if="isUserLoggedIn && isBookmarked"
+          dark
+          class="cyan"
+          @click="unbookmark"
+        >
+          Unbookmark
+        </v-btn>
       </v-flex>
       <v-flex xs6>
         <img class="album-cover" :src="song.albumImageUrl" />
@@ -37,11 +54,54 @@
 </template>
 
 <script>
+import {mapState} from 'vuex'
+import BookmarksService from '@/services/BookmarksService'
 
 export default {
   props: [
     'song'
-  ]
+  ],
+  data () {
+    isBookmarked: false
+  },
+  computed: {
+    ...mapState([
+      'isUserLoggedIn'
+    ])
+  },
+  async mounted () {
+    try {
+      const bookmark = (await BookmarksService.index({
+        songId: this.song.id,
+        userId: this.$store.state.user.id
+      })).data
+      this.isBookmarked = !!bookmark
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  methods: {
+    async bookmark () {
+      try {
+        await BookmarksService.post({
+          songId: this.song.id,
+          userId: this.$store.state.user.id
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async unbookmark () {
+      try {
+        await BookmarksService.delete({
+          songId: this.song.id,
+          userId: this.$store.state.user.id
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 }
 </script>
 
