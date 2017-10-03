5ea05d1d0c6f70235fa2fcce571bf0b1846f1631
MAshraf <ee.mashraf@gmail.com>
2017-10-03 15:49:05 +0200

    bookmark controller fix

--- server/src/controllers/BookmarksController.js
+++ server/src/controllers/BookmarksController.js
@@ -20,9 +20,23 @@ module.exports = {
   },
   async post (req, res) {
     try {
-      const bookmark = req.body
-      await Bookmark.create(bookmark)
-      res.send(bookmark)
+      const {songId, userId} = req.body
+      const bookmark = await Bookmark.findOne({
+        where: {
+          UserId: userId,
+          SongId: songId
+        }
+      })
+      if(bookmark){
+        return res.status(400).send({
+          error: 'you already have this as a bookmark'
+        })
+      }
+      const newBookmark = await Bookmark.create({
+        SongId: songId,
+        UserId: userId
+      })
+      res.send(newBookmark)
     } catch (err) {
       res.status(500).send({
         error: 'an error has occured trying to create the bookmark'
