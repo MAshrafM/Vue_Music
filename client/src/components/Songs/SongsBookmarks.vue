9a7366124baae888859a86f434d56c865161156d
MAshraf <ee.mashraf@gmail.com>
2017-10-04 17:13:43 +0200

    SongsBookmark vue

---
+++ client/src/components/Songs/SongsBookmarks.vue
@@ -0,0 +1,70 @@
+<template>
+  <panel title="Bookmarks">
+    <v-data-table
+      :headers="headers"
+      :pagination.sync="pagination"
+      :items="bookmarks"
+    >
+      <template slot="items" scope="props">
+        <td class="text-xs-right">
+          {{props.item.title}}
+        </td>
+        <td class="text-xs-right">
+          {{props.item.artist}}
+        </td>
+      </template>
+    </v-data-table>
+  </panel>
+</template>
+
+<script>
+import {mapState} from 'vuex'
+import BookmarksService from '@/services/BookmarksService'
+export default {
+  data () {
+    return {
+      headers: [
+        {
+          text: 'Title',
+          value: 'title'
+        },
+        {
+          text: 'Artist',
+          value: 'artist'
+        }
+      ],
+      pagination: {
+        sortBy: 'date',
+        descending: true
+      },
+      bookmarks: [
+        {
+          title: 'hell'
+          artist: 'Test'
+        },
+        {
+          title: 'hell0'
+          artist: 'Testoo'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapState([
+      'isUserLoggedIn',
+      'user'
+    ])
+  }
+  async mounted () {
+    if (this.isUserLoggedIn) {
+      this.bookmarks = (await BookmarksService.index({
+        userId: this.user.id
+      })).data
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
